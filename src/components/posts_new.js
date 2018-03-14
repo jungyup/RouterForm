a50a8527fbf5afa8faa8d91c1ffa89e614701dd2
Eddie Kim <jungyupkim4@gmail.com>
2018-03-13 23:58:54 -0400

    Fourth Commit
    Form and Validation added (not completed)

---
+++ src/components/posts_new.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class PostsNew extends Component {
+    renderField(field) {
+        return (
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    {...field.input}
+                />
+                {field.meta.error}
+            </div>
+        );
+    }
+
+    render() {
+        return (
+            <form>
+                <Field
+                    label="Title For Post"
+                    name="title"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Categories"
+                    name="categories"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Post Content"
+                    name="content"
+                    component={this.renderField}
+                />
+            </form>
+        );
+    }
+}
+
+function validate(values) {
+    const errors = {};
+
+    // Validate the inputs from 'values'
+    // if (values.title.length < 3) {
+    //     errors.title = 'Title must be at least 3 characters';
+    // }
+    if (!values.title) {
+        errors.title = "Enter a title!";
+    }
+    if (!values.categories) {
+        errors.categories = "Enter some categories";
+    }
+    if (!values.content) {
+        errors.content = 'Enter some content please';
+    }
+
+    // If errors is empty, the form is fine to submit
+    // If errors have *any* properties, redux form assumes form is invalid
+    return errors;
+}
+
+export default reduxForm({
+    validate,
+    form: 'PostsNewForm'
+})(PostsNew);
+
